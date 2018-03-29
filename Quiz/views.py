6e94208bfb15a350f225b15895bac792fac6ff79
admin <nutty1997@live.com>
2018-03-29 13:50:53 +0700

    Create Model

--- Quiz/views.py
+++ Quiz/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from Quiz.models import Question
 
 def home_page(request):
+    
     return render(request, 'home.html')
